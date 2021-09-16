5d67e3054a95b02f377f880eb0b6a58d3cafd56c
Rajan Prajapati <rajanprajapati1199@gmail.com>
2021-09-16 09:45:51 +0530

    Fix

--- main.go
+++ main.go
@@ -13,11 +13,11 @@ func main() {
 	fmt.Println("Welcome to This API Project of Cila Labs")
 	r := mux.NewRouter()
 
-	r.HandleFunc("/users", handler.CreateUser).Methods("POST")
-	r.HandleFunc("/users", handler.GetUsers).Methods("GET")
-	r.HandleFunc("/user/{id}", handler.GetUserByID).Methods("GET")
+	r.HandleFunc("/v1/users", handler.CreateUser).Methods("POST")
+	r.HandleFunc("/v1/users", handler.GetUsers).Methods("GET")
+	r.HandleFunc("/v1/user/{id}", handler.GetUserByID).Methods("GET")
 	//r.HandleFunc("/user/{id}", handler.UpdateUser).Methods("PUT")
-	r.HandleFunc("/user/{id}", handler.DeleteUser).Methods("DELETE")
+	r.HandleFunc("/v1/user/{id}", handler.DeleteUser).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 
